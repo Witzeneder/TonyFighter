6bebe8d772f3f622d53fa06aa05526e09a4f57f2
JakobSpi <jakob.spirk@gmx.net>
2017-06-28 16:40:46 +0200

    Commit 10
    Button Play is now starting the scene "endless" and button exit is closing the Application

---
+++ LonelyTony/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void PlayEndlessButton(string endlessGame)
+    {
+
+        SceneManager.LoadScene(endlessGame);
+    }
+
+    public void ExitAppButton()
+    {
+        Application.Quit();
+    }
+}
