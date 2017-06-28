e025195be599f76ee3a4acbc5ba8e5e1c76af870
ValentinWitz <s1610237037@students.fh-hagenberg.at>
2017-06-28 11:28:40 +0200

    Commit 5
    Added PlatformGeneration
    Added PlatformDestruction

---
+++ LonelyTony/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+	public GameObject platformDestruction;
+
+	// Use this for initialization
+	void Start () {
+
+		//Find the PlatformDestructionPoint --> finds it by name in the Object's
+		platformDestruction = GameObject.Find ("PlatformDestructionPoint");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if transform position is past the PlatformDestructionPoint
+		if (transform.position.x < platformDestruction.transform.position.x) {
+
+			Destroy (gameObject);
+		}
+		
+	}
+}
---
+++ LonelyTony/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform; 		//platform that is generated
+	public Transform generationPoint;	//the generation point
+	public float distanceBetween;		//the distance between two platforms
+	private float platformWidth;		//platform width
+
+	// Use this for initialization
+	void Start () {
+		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x; //get platform width
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.x < generationPoint.position.x) {
+
+			//get the new Position for the GenerationPoint
+			transform.position = new Vector3 (transform.position.x + platformWidth + distanceBetween,
+				transform.position.y, transform.position.z);											
+
+			//Instantiate = makes a new Instant of "thePlatform" -> handles the platform generation
+			Instantiate (thePlatform, transform.position, transform.rotation);
+
+		}
+
+	}
+}
