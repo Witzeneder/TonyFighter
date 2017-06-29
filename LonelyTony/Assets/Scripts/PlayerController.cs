a98aca573a6b1ad88279997bcffad6429290d045
HPin <H.PINSKER@LIVE.DE>
2017-06-30 00:16:15 +0200

    player controller
    Added enhanced touch recognition
    
    - JS
    - HP

--- LonelyTony/Assets/Scripts/PlayerController.cs
+++ LonelyTony/Assets/Scripts/PlayerController.cs
@@ -87,39 +87,44 @@ public class PlayerController : MonoBehaviour {
 	
 
 		//if Space is pressed or Mouse is pressed -> Jump
-		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {				//Click Button
+		//if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {				//Click Button
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
 
-			if (isOnGround) {
-				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
-				isJumping = true;
-			}
-
-		}
-
-		if ((Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) && isJumping) {			//pressed Button
-
-			if (jumpTimeCounter > 0) {
-				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);		//if pressed longer you can continue jumping
-				jumpTimeCounter -= Time.deltaTime;											//decrease jumpTimeCounter so it gets < 0 and stop jump
-			}
-		}
-
-		if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) {					//Release Button
-
-			jumpTimeCounter = 0;															//when stop pressing stop jumping
-			isJumping = false;
-
-		}
-
-			if(isOnGround) {																//if again on Ground reset Counter
-
-				jumpTimeCounter = jumpTime;
+				if (isOnGround) {
+					myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+					isJumping = true;
+				}
 
 			}
-			
+
+			//if ((Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) && isJumping) {			//pressed Button
+			if (Input.GetTouch (0).phase == TouchPhase.Moved || Input.GetTouch (0).phase == TouchPhase.Stationary) {
+
+				if (jumpTimeCounter > 0) {
+					myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);		//if pressed longer you can continue jumping
+					jumpTimeCounter -= Time.deltaTime;											//decrease jumpTimeCounter so it gets < 0 and stop jump
+				}
+			}
+
+			//if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) {					//Release Button
+			if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+
+				jumpTimeCounter = 0;															//when stop pressing stop jumping
+				isJumping = false;
+
+			}
+		}
+
+		if (isOnGround) {																//if again on Ground reset Counter
+
+			jumpTimeCounter = jumpTime;
+
+		}
+		
+				
 		playerAnimator.SetBool ("IsOnGround", isOnGround); // connect Animator parameter to our variable isOnGround
-
-			}
+	}
 
 
 	/*
