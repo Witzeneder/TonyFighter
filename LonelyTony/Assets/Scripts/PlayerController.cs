58625a82824cd171f41890098bf11a285b6faf69
Witzeneder <witzeneder.v@gmail.com>
2020-01-22 17:13:53 +0100

    CRC

--- LonelyTony/Assets/Scripts/PlayerController.cs
+++ LonelyTony/Assets/Scripts/PlayerController.cs
@@ -1,145 +1,160 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;						//Speed of the Player
-	private float moveSpeedStore;				//to set the moveSpeed back to normal after death
-	public float speedMultiplier; 				//for speed ups
-	public float speedPointMultiplier;			//for variable distance for speed increase points
-	public float speedUpPoint;					//distance when speed becomes increased
-	private float speedUpPointStore;			//for setting the speedUpPoint back to normal after death
-	private float countSpeedIncreases;			//= how often the speed has been increased
-	private float countSpeedIncreasesStore;		//set back how often the speed has been inceased
-
 	public float jumpForce;					//Jump Height of the Player
-	public float jumpTime;					//to configure the jump boost
-	private float jumpTimeCounter; 			//controll the player
-
-	private bool isJumping;			// true if player is currently in the air
-
-
-
-	//moveSpeed and jumpForce can be modified in the Player Inspector 
 
 	private Rigidbody2D myRigidbody;		//Object this script is working with
+    private Animator animator;
+    private bool facesRight;
+    public GameObject groundCheckGameObject;
+    private float groundThreshold;
 
-	public bool isOnGround;
-	public LayerMask whatIsGround;
-	public Transform checkGround;
-	public float checkGroundRadius;
-
-	/*Used for climbing walls
-	private Collider2D myCollider;
-	*/
-
-	private Animator playerAnimator;
-
-	public GameManager gameManager;
-
+    public GameObject Bullet_Emitter;
+    public GameObject Bullet;
+    public float Bullet_Force;
+    public float reloadTime = 2f;
+    private float bulletReloadTime;
 
 	//-------------------------------------------------------------------------------------------------------//
 
 	// Use this for initialization
 	void Start () {
 
-		myRigidbody = GetComponent<Rigidbody2D> (); //Get the Player Object
+        facesRight = true;
+        myRigidbody = GetComponent<Rigidbody2D> (); //Get the Player Object
+        animator = GetComponent<Animator>();
+        groundThreshold = groundCheckGameObject.transform.position.y;
 
-		/*Used for climbing walls
-		myCollider = GetComponent<Collider2D> ();	// find the collider that is attached to the object
-		*/
+    }
 
-		playerAnimator = GetComponent<Animator> ();
+    // Update is called once per frame
+    void Update()
+    {
+        if (bulletReloadTime > 0f)
+        {
+            bulletReloadTime -= Time.deltaTime;
+        }
 
-		jumpTimeCounter = jumpTime;
-		isJumping = false;
+        if (Input.GetMouseButton(0))
+        {
+            bool actionProcessed = false;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            if (hit)
+            {
 
-		countSpeedIncreases = speedUpPoint;			//otherwise the speed would already increase at the start
+                GameObject hitObject = hit.transform.gameObject;
 
-		moveSpeedStore = moveSpeed;
-		speedUpPointStore = speedUpPoint;
-		countSpeedIncreasesStore = countSpeedIncreases;
+                if (hitObject.name == "jump")
+                {
+                    jumpButtonClicked();
+                    actionProcessed = true;
+                }
 
-	}
+                else if (hitObject.name == "shoot")
+                {
+                    if (bulletReloadTime < 0.01f)
+                    {
+                        fireShot();
+                        bulletReloadTime = reloadTime;
+                    }
+                    actionProcessed = true;
+                }
+            }
 
-	// Update is called once per frame
-	void Update () {
+            if (!actionProcessed)
+            {
+                if (Input.mousePosition.x > Screen.width * 0.5f)
+                {
+                    moveRight();
+                }
+                else
+                {
+                    moveLeft();
+                }
+            }
 
+        }
+        else if (groundCheckGameObject.transform.position.y <= groundThreshold)
+        {
+            animator.Play("Empty_State");
+        }
+        else
+        {
+            animator.Play("Player_Jump");
+        }
+    }
 
+    public void moveLeft()
+    {
+        if (facesRight)
+        {
+            myRigidbody.transform.Rotate(0f, 180f, 0f);
+        }
+        facesRight = false;
+        myRigidbody.transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        animator.Play("Player_Run");
+    }
 
-		/*Used for climbing walls
-		isOnGround = Physics2D.IsTouchingLayers (myCollider, whatIsGround); // checks if player's collider is touching ground's collider
-		*/
+    public void moveRight()
+    {
+        if (!facesRight)
+        {
+            myRigidbody.transform.Rotate(0f, 180f, 0f);
+        }
+        facesRight = true;
+        myRigidbody.transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        animator.Play("Player_Run");
+    }
 
-		isOnGround = Physics2D.OverlapCircle (checkGround.position, checkGroundRadius, whatIsGround);
+    public void jumpButtonClicked()
+    {
+        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        animator.Play("Player_Jump");
+    }
 
-		if (transform.position.x > countSpeedIncreases) {					//increase speed when a speed up point has been reached
+    public void fireShot()
+    {
+        GameObject bullet = Instantiate(Bullet, Bullet_Emitter.transform.position,Bullet_Emitter.transform.rotation) as GameObject;
 
-			countSpeedIncreases = countSpeedIncreases + speedUpPoint;
-			speedUpPoint = speedUpPoint * speedPointMultiplier;				//speed points have to be farther apart as the player becomes faster
-			moveSpeed = moveSpeed * speedMultiplier;
-
-		}
-
-
-		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y); //X,Y Vector
-	
-
-		//if Space is pressed or Mouse is pressed -> Jump
-		//if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {				//Click Button
-		if (Input.touchCount > 0 ) {
-			if (Input.GetTouch (0).phase == TouchPhase.Began ) {
-
-				if (isOnGround) {
-					myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
-					isJumping = true;
-				}
-
-			}
-
-			//if ((Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) && isJumping) {			//pressed Button
-			if ((Input.GetTouch (0).phase == TouchPhase.Moved || Input.GetTouch (0).phase == TouchPhase.Stationary) && isJumping) {
-
-				if (jumpTimeCounter > 0) {
-					myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);		//if pressed longer you can continue jumping
-					jumpTimeCounter -= Time.deltaTime;											//decrease jumpTimeCounter so it gets < 0 and stop jump
-				}
-			}
-
-			//if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) {					//Release Button
-			if (Input.GetTouch (0).phase == TouchPhase.Ended) {
-
-				jumpTimeCounter = 0;															//when stop pressing stop jumping
-				isJumping = false;
-
-			}
-		}
-
-		if (isOnGround) {																//if again on Ground reset Counter
-
-			jumpTimeCounter = jumpTime;
-
-		}
-		
-				
-		playerAnimator.SetBool ("IsOnGround", isOnGround); // connect Animator parameter to our variable isOnGround
-	}
-
-
-	/*
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+        bulletRigidbody.AddForce(transform.right * Bullet_Force);
+        Destroy(bullet, 1.5f);
+    }
+   
+    /*
 	 * Predefined unity function, used to recognize if 2 colliders touch each other
 	 * Here: if player falls down and dies
 	 */
-	void OnCollisionEnter2D (Collision2D other) {
-		if (other.gameObject.tag == "killbox") {
-			Debug.Log ("Player dead");							//Console shows Player dead
-			gameManager.RestartGame ();							//Restart method from GameManager
-			moveSpeed = moveSpeedStore;							//reset the moveSpeed		
-			speedUpPoint = speedUpPointStore;					//reset the speedUpPoint
-			countSpeedIncreases = countSpeedIncreasesStore;
-		}
+    void OnCollisionEnter2D (Collision2D other) {
+       
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.name == "bulletPowerUp(Clone)")
+        {
+            reloadTime -= 0.5f;
+            if (reloadTime < 0)
+            {
+                reloadTime = 0f;
+            }
+            bulletReloadTime = 0f;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.name == "heart(Clone)")
+        {
+            //GIVE LIFE
+            Destroy(collision.gameObject);
+        }
+    }
+
 
 }
 
---
+++ LonelyTony/Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+
+
+    public GameObject bulletPowerUp;
+    public GameObject lifePowerUp;
+
+    private List<GameObject> powerUps;
+    private readonly System.Random rnd = new System.Random();
+    public float powerUpDuration = 10f;
+    private float time;
+    private GameObject activeGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerUps = new List<GameObject>
+        {
+            bulletPowerUp,
+            lifePowerUp
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (activeGameObject != null)
+        {
+            time = 0.0f;
+            return;
+        }
+        if (time >= powerUpDuration)
+        {
+            time = 0.0f;
+
+            GameObject powerUp = GetRandomPowerUp();
+            SpawnGameObject(powerUp);
+        }
+    }
+
+
+    private void SpawnGameObject(GameObject powerUp)
+    {
+        float x = Random.Range(-12f, 4f);
+        float y = Random.Range(-1.5f, 3.3f);
+        Vector3 position = new Vector3(x, y, 0f);
+        Quaternion rotation = powerUp.transform.rotation;
+        activeGameObject = Instantiate(powerUp, position, rotation);
+        Debug.Log(x);
+    }
+
+    private GameObject GetRandomPowerUp()
+    {
+        int randomIndex = rnd.Next(powerUps.Count);
+        return powerUps[randomIndex];
+    }
+
+
+}
